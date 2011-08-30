ecb60464c8b58579eb22c5294e41d0802955b6cd
Evan Dudla <me@evandudla.com>
2011-08-30 00:28:30 -0400

    Added dummy home view

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <head>
+    <title><?php echo APP_NAME; ?></title>
+  </head>
+  <body>
+    <h1><?php echo APP_NAME; ?></h1>
+    <p>This is my application written in the Featherweight framework.</p>
+  </body>
+</html>
