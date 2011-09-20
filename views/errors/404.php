f1096173b841e473a5c38d4aa973ab1637ec7a0b
Evan Dudla <me@evandudla.com>
2011-09-19 23:47:35 -0400

    Added 404 error page

---
+++ views/errors/404.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <head>
+    <title><?php echo APP_NAME; ?></title>
+  </head>
+  <body>
+    <h1>An error occurred</h1>
+    <p>We cannot seem to find the page you have requested!</p>
+  </body>
+</html>
