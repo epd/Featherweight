4739baee480307641933b58aa254a2f4f8740458
Evan Dudla <me@evandudla.com>
2011-09-29 01:48:06 -0400

    Fixed convert string to boolean for DEBUG override

--- system/init.php
+++ system/init.php
@@ -8,9 +8,25 @@
  * PHP constants to be used throughout the web app. It also performs any
  * other necessary initialization steps and pre-routing processing.
  */
+
+// Override $debug global if environment variable set (convert string to boolean)
+if (isset($_SERVER['DEBUG'])) {
+  switch (strtoupper($_SERVER['DEBUG'])) {
+    case "TRUE":
+      $debug = TRUE;
+      break;
+    case "FALSE":
+      $debug = FALSE;
+      break;
+    default:
+      break;
+  }
+}
+
+// Our definitions
 define("APP_NAME", $application['app name']);
 define("BASE_PATH", $application['base path']);
-define("DEBUG", isset($_SERVER['DEBUG']) ? $_SERVER['DEBUG'] : (isset($debug) ? $debug : FALSE));
+define("DEBUG", isset($debug) ? $debug : FALSE);
 define("THEME", isset($theme) ? $theme : "default");
 define("TEMPLATE", isset($template) ? $template : "template.php");
 define("APP_CSS", "/theme/" . THEME . "/css");
