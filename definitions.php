2211b6c77008523c5d2af52f28218b68ba69c85f
Evan Dudla <me@evandudla.com>
2011-11-17 16:05:24 -0500

    Added README/License to project

--- definitions.php
+++ definitions.php
@@ -41,3 +41,17 @@ $router["/example"] = array(
   "view" => "example.php",
 );
 
+// Define data sets
+$data['KEY'] = array(
+  "table1" => array(
+    "field1",
+    "field2",
+    "field3",
+  ),
+  "conditions" => array(
+    "OR" => array(
+      "field1 = field2",
+      "field1 = 1",
+    ),
+  ),
+);
